257e018eb60050bfaf2bb0a2f7c85d47c058d64f
VaradVithalKj <47701278+DoomslayerKj@users.noreply.github.com>
2020-11-24 15:24:28 +0530

    Add files via upload

---
+++ Lab7/pgm2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+read -p "Enter User Name: " name
+
+exist=$(cat /etc/passwd | grep -i $name | wc -l)
+flag=0
+
+
+if [ $exist -eq 0 ]; then
+	echo "User does not exist!! "
+	flag=1
+	exit 1
+else
+
+	if [ $flag -eq 1 ];then
+		echo ""
+	else
+		
+		cat /etc/passwd | grep -w $name | cut -d ":" -f6
+	fi
+
+	
+fi
